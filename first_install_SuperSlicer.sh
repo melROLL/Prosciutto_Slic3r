0afa31295ec6caf2c32d3a3e56fe3d3ba18d9c08
melROLL <60772441+melROLL@users.noreply.github.com>
2022-10-28 15:06:31 +0100

    Update first_install_SuperSlicer.sh

--- first_install_SuperSlicer.sh
+++ first_install_SuperSlicer.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
+#cool colors go brrrrrrr
+function echo_yellow {
+  TEXT="${@}"
+  echo -e "\e[33m${TEXT}\e[0m"
+}
+function echo_green {
+  TEXT="${@}"
+  echo -e "\e[32m${TEXT}\e[0m"
+}
+function echo_red {
+  TEXT="${@}"
+  echo -e "\e[31m${TEXT}\e[0m"
+}
+
+
 sudo apt update
 wait
 sudo apt install git 
 wait
 cd
-echo "===================="
-echo "= coffee break n°1 ="
-echo "===================="
+echo_green "===================="
+echo_yellow "= coffee break n°1 ="
+echo_green "===================="
 git clone https://github.com/supermerill/SuperSlicer.git
 sudo apt upgrade -y
 wait
 cd SuperSlicer
-echo "===================="
-echo "= coffee break n°2 ="
-echo "===================="
+echo_green "===================="
+echo_yellow "= coffee break n°2 ="
+echo_green "===================="
 sudo ./BuildLinux.sh -u
 wait
-echo "===================="
-echo "= coffee break n°2 ="
-echo "===================="
+echo_green "===================="
+echo_yellow "= coffee break n°2 ="
+echo_green "===================="
 sudo ./BuildLinux.sh -dsi
 wait
-echo "===================="
-echo "=      finish      ="
-echo "===================="
+echo_yellow "===================="
+echo_green "===================="
+echo_yellow "=      finish      ="
+echo_green "===================="
+echo_yellow "===================="
