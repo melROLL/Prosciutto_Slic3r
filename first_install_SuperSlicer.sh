173696de768654017db5cd362a74f0edab9a694a
melROLL <60772441+melROLL@users.noreply.github.com>
2022-10-28 13:45:18 +0100

    Create first_install_SuperSlicer.sh

---
+++ first_install_SuperSlicer.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt install git 
+cd
+echo "===================="
+echo "= coffee break n°1 ="
+echo "===================="
+git clone https://github.com/supermerill/SuperSlicer.git
+cd SuperSlicer
+echo "===================="
+echo "= coffee break n°2 ="
+echo "===================="
+sudo ./BuildLinux.sh -u
+echo "===================="
+echo "= coffee break n°2 ="
+echo "===================="
+./BuildLinux.sh -dsi
+echo "===================="
+echo "=      finish      ="
+echo "===================="
