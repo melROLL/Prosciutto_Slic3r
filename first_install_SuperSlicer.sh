fe3ccc05b70047df5157dde88d69691683894522
melROLL <60772441+melROLL@users.noreply.github.com>
2022-10-28 13:50:09 +0100

    Update first_install_SuperSlicer.sh

--- first_install_SuperSlicer.sh
+++ first_install_SuperSlicer.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 sudo apt update
+wait
 sudo apt install git 
+wait
 cd
 echo "===================="
 echo "= coffee break n°1 ="
 echo "===================="
 git clone https://github.com/supermerill/SuperSlicer.git
+wait
 cd SuperSlicer
 echo "===================="
 echo "= coffee break n°2 ="
 echo "===================="
 sudo ./BuildLinux.sh -u
+wait
 echo "===================="
 echo "= coffee break n°2 ="
 echo "===================="
 ./BuildLinux.sh -dsi
+wait
 echo "===================="
 echo "=      finish      ="
 echo "===================="
