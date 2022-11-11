d64e6a39062a6a98ec7b9d5b79ef05b06aaf252d
MEL <60772441+melROLL@users.noreply.github.com>
2022-11-11 22:19:12 +0100

    Update first_install_SuperSlicer_or_Mortadella_Slic3r.sh

--- first_install_SuperSlicer_or_Mortadella_Slic3r.sh
+++ first_install_SuperSlicer_or_Mortadella_Slic3r.sh
@@ -15,7 +15,27 @@ function echo_red {
 }
 
 #chose if the user want to buils SuperSlicer of Mortadella Slicer
-read -r -p "Do you want to install SuperSlicer or Mortadella_Slicer? [S/M] " response
+read -r -p "Do you want to install Bee_Slic3r (B), Don't do anything (D), install SuperSlicer (S), install Mortadella_Slicer (M) ? [B/D/S/M] " response
+
+case "$response" in
+    [sS][eE][sS]|[bB]) 
+      sudo apt update
+wait
+sudo apt install git -y
+wait
+cd
+echo_green "===================="
+echo_yellow "= coffee break n°1 ="
+echo_green "===================="
+sudo apt upgrade -y
+wait
+sudo chmod +x Build_Bee_Slic3r.sh
+      git clone https://github.com/melROLL/Bee_Slic3r -y
+      wait
+cd Bee_Slic3r
+        ;;
+    *)
+
 case "$response" in
     [sS][eE][sS]|[sS]) 
       sudo apt update
@@ -34,6 +54,8 @@ sudo chmod +x Build_SuperSlicer.sh
 cd SuperSlicer
         ;;
     *)
+    case "$response" in
+    [sS][eE][sS]|[mM]) 
         sudo apt update
 wait
 sudo apt install git -y
@@ -48,7 +70,10 @@ sudo chmod +x Build_Mortadella_Slic3r.sh
         git clone https://github.com/melROLL/Mortadella_Slic3r -y
         wait
 cd Mortadella_Slic3r
+        ;;
+    *)
 
+echo 'Understandable have a great day'
         ;;
 esac
 
