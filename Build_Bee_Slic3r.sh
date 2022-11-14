2b7c67bfe83bdec1debe13ae76c287d99d02f34d
MEL <60772441+melROLL@users.noreply.github.com>
2022-11-14 17:25:38 +0100

    Update Build_Bee_Slic3r.sh

--- Build_Bee_Slic3r.sh
+++ Build_Bee_Slic3r.sh
@@ -5,11 +5,6 @@ chmod +x BuildLinux.sh
 echo "===================="
 echo "= coffee break n°1 ="
 echo "===================="
-sudo ./BuildLinux.sh -u
-wait
-echo "===================="
-echo "= coffee break n°2 ="
-echo "===================="
 sudo ./BuildLinux.sh -dsi
 wait
 echo "===================="
